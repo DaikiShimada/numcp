c0fd0c25c3db2b9e41e6091ade9345c835adeff9
daiki_shimada_mba <sheema.sheemap@gmail.com>
2015-10-06 02:27:38 +0900

    opencv_exampleの追加

---
+++ example/opencv_example.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <numcp.h>
+#include <numcp_opencv.hpp>
+
+int main(int argc, char const* argv[])
+{
+	cv::Mat in_img = cv::imread("data/misc/test_rgb.tiff", 1);
+	std::cout << in_img << std::endl << std::endl;
+	return 0;
+}
